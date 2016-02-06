600b4bea972d02f474a5f4fd8c995355e0133d92
Samantha Bhuiyan <sbhuiyan802@hotmail.com>
2016-02-06 13:45:16 -0800

    git is dumb

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,6 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 600, 1);
+        addObject(new SpeechBubble("play-bubble.png", forest, 1000), -500, -500);
     }
 }
